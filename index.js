ca0ca9c9a1d2836b2f44e12d1b3e39d5a418e9f7
manoj <manojthiwari890@gmail.com>
2023-11-06 13:56:32 +0530

    proces env port added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8080
+const port = process.env.PORT || 8080
 const VERSION = require("./package.json").version
 
 app.listen(port, (req, res) => {
